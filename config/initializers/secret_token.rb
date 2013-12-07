bd2dfdf6bed73ac0e57a319ce823632c4fa1d406
Jayan Fazal Karim <jfkarim@172-29-27-250.dynapool.nyu.edu>
2013-12-06 21:37:19 -0500

    first (real) commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookFinder::Application.config.secret_token = '685d1e94f605e12e65c204b7bbd3f373add8c41322d389a110be44b8da1af01c60f5166815ceaeecda64ca036c431fc8ff0883ab42cae2f0918c30e04d21ea8e'
---
+++ db/migrate/20131207021256_create_books.rb
@@ -0,0 +1,22 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :college_name
+      t.string :dept_name
+      t.string :major #of person who checked book out
+      t.string :bib_author
+      t.string :bib_title
+      t.string :call_number_major #location of book (beginning of call number)
+
+      #fields in data-file COLLEGE_NAME, DEPT_NAME, MAJOR (OF PERSON CHECKED), BIB_AUTHOR, BIB_TITLE, CALL_NUMBER_MAJOR
+
+      t.timestamps
+    end
+    add_index :books, :college_name
+    add_index :books, :dept_name
+    add_index :books, :major
+    add_index :books, :bib_author
+    add_index :books, :bib_title
+    add_index :books, :call_number_major
+  end
+end
